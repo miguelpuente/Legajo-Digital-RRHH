71c3359df2df35a0f61871301f962e1d8bd6fd87
[PuenteMiguel] <[wilo1985@hotmail.com]>
2022-07-29 18:08:20 -0300

    Update route index

--- src/modules/routersIndex.js
+++ src/modules/routersIndex.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const auth = require('./auth/router/auth')
 const users = require('./users/users.router')
+const empresas = require('./empresas/empresas.router')
 
 const router = express.Router()
 
 router.use('/auth', auth)
 router.use('/users', users)
+router.use('/empresas', empresas)
 
 
 module.exports = router
