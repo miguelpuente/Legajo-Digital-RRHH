d85fc7e7de048214a90a4eadc89019ca16bcce06
[PuenteMiguel] <[wilo1985@hotmail.com]>
2022-08-10 17:00:21 -0300

    Add config production

--- src/config/config.js
+++ src/config/config.js
@@ -18,10 +18,12 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DB_USER_PROD,
+    "password": process.env.DB_PASSWORD_PROD,
+    "database": process.env.DB_NAME_PROD,
+    "host": process.env.DB_HOST_PROD,
+    "port": process.env.DB_PORT_PROD,
+    "dialect": process.env.DIALECT_PROD,
+    "keyJWT": process.env.KEYJWT_PROD
   }
 }
